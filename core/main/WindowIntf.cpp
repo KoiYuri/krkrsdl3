a88142e4958a5948819e6781eb11c235e6ce31d9
lxl <1403586197@qq.com>
2026-08-19 16:59:51 +0800

    添加utf-8与unicode之间统一的转化函数。

--- core/main/WindowIntf.cpp
+++ core/main/WindowIntf.cpp
@@ -543,25 +543,8 @@ void tTJSNI_BaseWindow::OnKeyPress(tjs_uint16 key)
         return;
     if (Owner)
     {
-        tjs_char buf[4];
-        if (key < 0x80)
-        {
-            buf[0] = (tjs_char)key;
-            buf[1] = 0;
-        }
-        else if (key < 0x800)
-        {
-            buf[0] = (tjs_char)(0xC0 | (key >> 6));
-            buf[1] = (tjs_char)(0x80 | (key & 0x3F));
-            buf[2] = 0;
-        }
-        else
-        {
-            buf[0] = (tjs_char)(0xE0 | (key >> 12));
-            buf[1] = (tjs_char)(0x80 | ((key >> 6) & 0x3F));
-            buf[2] = (tjs_char)(0x80 | (key & 0x3F));
-            buf[3] = 0;
-        }
+        tjs_char buf[5];
+        TJS_unicode_to_utf8(key, buf);
         tTJSVariant arg[1] = {buf};
         static ttstr eventname(TJS_N("onKeyPress"));
         TVPPostEvent(Owner, Owner, eventname, 0, TVP_EPT_IMMEDIATE, 1, arg);
--- plugins/Kirikiroid2/kirikiroid2.cpp
+++ plugins/Kirikiroid2/kirikiroid2.cpp
@@ -30,32 +30,9 @@ tjs_error krkr_str_ord(tTJSVariant* result,
         if (type.Type() == tvtString)
         {
             const tTJSVariantString* vs = type.AsStringNoAddRef();
-            const unsigned char* p = reinterpret_cast<const unsigned char*>(vs->ShortString);
-            if (!*p)
+            if (!vs)
                 return 0;
-            int tmp = 0;
-            if ((*p & 0x80) == 0)
-            {
-                tmp = *p;
-            }
-            else if ((*p & 0xE0) == 0xC0)
-            {
-                tmp = ((*p & 0x1F) << 6) | (p[1] & 0x3F);
-            }
-            else if ((*p & 0xF0) == 0xE0)
-            {
-                tmp = ((*p & 0x0F) << 12) | ((p[1] & 0x3F) << 6) | (p[2] & 0x3F);
-            }
-            else if ((*p & 0xF8) == 0xF0)
-            {
-                tmp = ((*p & 0x07) << 18) | ((p[1] & 0x3F) << 12) | ((p[2] & 0x3F) << 6) |
-                      (p[3] & 0x3F);
-            }
-            else
-            {
-                tmp = 0;
-            }
-            *result = (tjs_int)tmp;
+            *result = (tjs_int)TJS_utf8_to_unicode((const tjs_char*)*vs);
         }
         else
         {
